a78c948edb89653dbdf94676ac83162c74699830
NCSaketh <nc.saketh1@gmail.com>
2020-09-28 01:26:35 +0530

    adding UC2

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -12,6 +12,12 @@ public class LineComparison {
 		int y3 = (int) (Math.floor(Math.random()*10)) ;
 		int x4 = (int) (Math.floor(Math.random()*10)) ;
 		int y4 = (int) (Math.floor(Math.random()*10)) ;
+		
+		length1 = (int)  Math.sqrt(Math.pow(x2-x1, 2)+ Math.pow(y2-y1, 2));
+		length2 = (int)  Math.sqrt(Math.pow(x4-x3, 2)+ Math.pow(y4-y3, 2));
+		
+		System.out.println("length1 " +x1 +"," +y1+ " and "+x2 +"," +y2 + " is " +length1 );
+		System.out.println("length2 " +x3 +"," +y3+ " and "+x4 +"," +y4 + " is " +length2 );
 	}
 		
 }
