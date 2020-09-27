4b8c6531b31a779240e4d1fd204ad546e424ac2d
NCSaketh <nc.saketh1@gmail.com>
2020-09-28 01:32:53 +0530

    adding UC4

--- src/LineComparison.java
+++ src/LineComparison.java
@@ -22,13 +22,13 @@ public class LineComparison {
 		Double l1 = Double.valueOf(length1);
 		Double l2 = Double.valueOf(length2);
 		
-		boolean compareValue = l1.equals(l2);
+		int m = l1.compareTo(l2);
 		
-		if (compareValue)
+		if (m == 0)
 			System.out.println("Both lines are equal");
 		else
 			System.out.println("Both lines are not equal");
-	}
+		}
 		
 }
 
